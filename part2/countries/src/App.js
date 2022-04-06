8fa249b00d88512b54fd863f202a4bb62e27d328
cdpaiva <cdpaivaa@gmail.com>
2022-04-05 23:16:35 -0400

    Countries exercise update

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,33 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
 function App() {
+  const URL = 'https://restcountries.com/v3.1/all'
+
+  const [countries, setCountries] = useState([])
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then(res => setCountries(res.data))
+  })
+
+  const handleChange = (e) => {
+    console.log('Called handleChange')
+    setQuery(e.target.value) 
+  }
+
+  const filteredCountries = countries.filter(c => c.name.common.includes(query))
+  
   return (
     <div>
-      <h1>New project</h1>
+      <h1>Fetch all countries from the {URL}</h1>
+      <label htmlFor='searchCountry'></label>
+      <input id='searchCountry' type='text' value={query} onChange={handleChange}/>
+      <ul>
+        <li>'Number of results: ',{filteredCountries.length})</li>
+      </ul>
     </div>
   );
 }
